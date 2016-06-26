511a4f92e6379b1c5a4f546c35c79832c45c1d29
Shiyu <spadillezhang@gmail.com>
2016-06-26 14:48:13 -0400

    initialize trucks

---
+++ Assets/Scripts/truck.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class truck  {
+	public int capacity;
+
+	public truck(){
+		this.capacity = 100;
+		Debug.Log ("this is a car with 100 cap");
+	}
+}
+
+public class RedTruck:truck{
+	public bool isActiveRed;
+	public RedTruck(){
+		isActiveRed = true;
+	}
+}
+
+public class BlueTruck:truck{
+	public bool isActiveBlue;
+	public BlueTruck(){
+		isActiveBlue = true;
+	}
+}
+
+public class GreenTruck:truck{
+	public bool isActiveGreen;
+	public GreenTruck(){
+		isActiveGreen = true;
+	}
+}
