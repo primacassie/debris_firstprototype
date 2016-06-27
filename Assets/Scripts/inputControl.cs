f5fd624c42112162bd5bf79bf7b80e4645ee4195
Shiyu <spadillezhang@gmail.com>
2016-06-26 20:17:30 -0400

    first simple version

--- Assets/Scripts/inputControl.cs
+++ Assets/Scripts/inputControl.cs
@@ -22,6 +22,25 @@ public class inputControl : MonoBehaviour {
 				Node.modifyCap (capVal, Node.passNode1, Node.passNode2);
 				Debug.Log("you collect "+ capVal +" the capacity of this path "+Node.passNode1
 					+ Node.passNode2+" remains: " +  gameControll.capArray[Node.passNode1,Node.passNode2]);
+				//if the node is the depot then reset it.
+				if (Node.passNode2 == 1 ) {
+					if (gameControll.blueTruck) {
+						gameControll.blueTruck = false;
+					}
+					if (gameControll.redTruck) {
+						gameControll.redTruck = false;
+					}
+					if (gameControll.greenTruck) {
+						gameControll.greenTruck = false;
+					}
+					Debug.Log (" You have finish a cycle, please start another one!");
+
+					//reset most of the things to the beginning here
+					gameControll.twoNode.Clear ();
+					GameObject.Find ("GameController").GetComponent<gameControll> ().resetCursor ();
+					GameObject.Find ("GameController").GetComponent<gameControll> ().resetDepot ();
+				}
+
 				GameObject.Find("InputTab").SetActive(false);
 			}
 		}catch(FormatException){
--- Assets/Scripts/redTruck.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class redTruck{
-	private int capacity;
-	public redTruck(){
-		//this.capacity = capacity;
-	}
-}
