5c2602e7685eb19d4fdd37d59605b2ef9b72e43d
shiyu zhang <shiyu zhang>
2016-08-08 00:20:19 -0400

    set filter can exist only one

--- Assets/Scripts/StoreTruckClick.cs
+++ Assets/Scripts/StoreTruckClick.cs
@@ -17,20 +17,24 @@ public class StoreTruckClick : MonoBehaviour {
 	public static float numForProfit;
 	public static float numForTime;
 	private int numClick=0;
+    //public static bool onlyOneClick;
 	//public static Queue<GameObject> objQueue=new Queue<GameObject>();
 	void OnMouseDown(){
-		numClick++;
+        numClick++;
 		//materialQueue.Clear ();
 		if (!(gameControll.redTruck || gameControll.greenTruck || gameControll.blueTruck)) {
-			//indicatorFunction.setCancelPathButton ();
-			if (numClick % 2 == 1) {
+            //indicatorFunction.setCancelPathButton ();
+            List<int> nodePathL = new List<int>();
+            List<int> nodeTrcukCap = new List<int>();
+            if (numClick % 2 == 1 && !(red || green || blue)) {
+                //onlyOneClick = true;
 				string truckName = this.gameObject.name;
 				string resultString = Regex.Match(truckName, @"\d+").Value;
 				int truckNum = int.Parse (resultString);
 				theTruckNum = truckNum;
 				//objQueue.Enqueue (this.gameObject);
-				List<int> nodePathL = new List<int> ();
-				List<int> nodeTrcukCap = new List<int> ();
+				//List<int> nodePathL = new List<int> ();
+				//List<int> nodeTrcukCap = new List<int> ();
 				float profit = 0f;
 				float time = 0f;
 				if (truckName [0] == 'r') {
@@ -284,40 +288,92 @@ public class StoreTruckClick : MonoBehaviour {
 			}
 
 			if (numClick % 2 == 0) {
-				int[] arr = Node.nodeArray;
-				for (int j = 0; j < arr.Length-1; j++) {
-					int num1 = arr [j];
-					int num2 = arr [j + 1];
-					string pathString = "pathAnim" + num1.ToString () + num2.ToString ();
-					GameObject obj = GameObject.Find (pathString);
-					//					obj.GetComponent<LineRenderer> ().material = Resources.Load<Material> ("Materials/greenAnim") as Material;
-					//					obj.GetComponent<LineRenderer> ().SetWidth (0.25f, 0.25f);
-					//string materialName=StoreTruckClick.materialQueue.Dequeue();
-					obj.GetComponent<LineRenderer> ().SetWidth (0.15f, 0.15f);
-					string truckText1 = "truckCap" + num1.ToString () + num2.ToString ();
-					string truckText2 = "truckCap" + num2.ToString() + num1.ToString ();
-					GameObject truckCap = new GameObject ();
-					if (GameObject.Find (truckText1) != null) {
-						truckCap = GameObject.Find (truckText1);
-					} else if (GameObject.Find (truckText2) != null) {
-						truckCap = GameObject.Find (truckText2);
-					}
-					Text t = truckCap.GetComponent<Text> ();
-					t.enabled = false;
-				}
-				if (StoreTruckClick.red) {
-					//string goName = "redTruckImage" +(gameControll.redTruckNum-1).ToString ();
-					//string goText = "redTruckText" + gameControll.redTruckNum.ToString ();
-					StoreTruckClick.red = false;
-				}
-				if (StoreTruckClick.green) {
-					StoreTruckClick.green = false;
-				}
-				if (StoreTruckClick.blue) {
-					StoreTruckClick.blue = false;
-				}
-				Destroy (GameObject.Find ("cancelMark"));
-				hasCancelMark = false;
+                string truckName = this.gameObject.name;
+                string resultString = Regex.Match(truckName, @"\d+").Value;
+                int truckNum = int.Parse(resultString);
+                if ((truckName[0]=='r' && red) || (truckName[0] == 'g' && green)|| (truckName[0] == 'b' && blue))
+                {
+                    if (truckName[0] == 'r')
+                    {
+                        int i = 0;
+                        foreach (List<int> l in Node.redAl)
+                        {
+                            if (i == truckNum)
+                            {
+                                nodePathL = l;
+                                break;
+                            }
+                            i++;
+                        }
+                    }
+                    if (truckName[0] == 'g')
+                    {
+                        int i = 0;
+                        foreach (List<int> l in Node.greenAl)
+                        {
+                            if (i == truckNum)
+                            {
+                                nodePathL = l;
+                                break;
+                            }
+                            i++;
+                        }
+                    }
+                    if (truckName[0] == 'b')
+                    {
+                        int i = 0;
+                        foreach (List<int> l in Node.blueAl)
+                        {
+                            if (i == truckNum)
+                            {
+                                nodePathL = l;
+                                break;
+                            }
+                            i++;
+                        }
+                    }
+                    int[] arr = nodePathL.ToArray();
+                    for (int j = 0; j < arr.Length - 1; j++)
+                    {
+                        int num1 = arr[j];
+                        int num2 = arr[j + 1];
+                        string pathString = "pathAnim" + num1.ToString() + num2.ToString();
+                        GameObject obj = GameObject.Find(pathString);
+                        //					obj.GetComponent<LineRenderer> ().material = Resources.Load<Material> ("Materials/greenAnim") as Material;
+                        //					obj.GetComponent<LineRenderer> ().SetWidth (0.25f, 0.25f);
+                        //string materialName=StoreTruckClick.materialQueue.Dequeue();
+                        obj.GetComponent<LineRenderer>().SetWidth(0.15f, 0.15f);
+                        string truckText1 = "truckCap" + num1.ToString() + num2.ToString();
+                        string truckText2 = "truckCap" + num2.ToString() + num1.ToString();
+                        GameObject truckCap = new GameObject();
+                        if (GameObject.Find(truckText1) != null)
+                        {
+                            truckCap = GameObject.Find(truckText1);
+                        }
+                        else if (GameObject.Find(truckText2) != null)
+                        {
+                            truckCap = GameObject.Find(truckText2);
+                        }
+                        Text t = truckCap.GetComponent<Text>();
+                        t.enabled = false;
+                    }
+                    if (StoreTruckClick.red)
+                    {
+                        //string goName = "redTruckImage" +(gameControll.redTruckNum-1).ToString ();
+                        //string goText = "redTruckText" + gameControll.redTruckNum.ToString ();
+                        StoreTruckClick.red = false;
+                    }
+                    if (StoreTruckClick.green)
+                    {
+                        StoreTruckClick.green = false;
+                    }
+                    if (StoreTruckClick.blue)
+                    {
+                        StoreTruckClick.blue = false;
+                    }
+                    Destroy(GameObject.Find("cancelMark"));
+                    hasCancelMark = false;
+                }
 			}
 		}
 	}
---
+++ Assets/Scripts/sliderClick.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class sliderClick : MonoBehaviour {
+    public static string sliderName;
+    void OnMouseDown()
+    {
+        sliderName = this.gameObject.name;
+        //Debug.Log(sliderName);
+    }
+}
